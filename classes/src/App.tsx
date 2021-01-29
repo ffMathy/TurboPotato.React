1c14a0772ec400700cca0cf5ae603c50d3efe60d
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-01-29 12:47:00 +0100

    minor tweak.

--- classes/src/App.tsx
+++ classes/src/App.tsx
@@ -2,25 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import classes from './App.module.scss';
 
-function App() {
-  return (
-    <div className={classes.App}>
-      <header className={classes['App-header']}>
-        <img src={logo} className={classes['App-logo']} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={classes['App-link']}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className={classes.App}>
+        <header className={classes['App-header']}>
+          <img src={logo} className={classes['App-logo']} alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className={classes['App-link']}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
